93a0da6c4a9c51bf9b865d7a662b03f5ea7f21f4
Arem <tgz57031@gmail.com>
2023-05-04 21:33:59 -0500

    variables, concatnacion, listas y ubicaion semejante a js

---
+++ variables.py
@@ -0,0 +1,14 @@
+
+numero = 809
+#concatenar con numeros, f lo convierte en string
+saludo = f"estamos agregando el {numero} como un string"
+
+print(saludo)
+#operadores de pertenencia, si esta o no dentro=falso o true
+print("lolo" in saludo)#esto es falso, porque lolo no esta
+print("tatiana" not in saludo)#verdadero, porque tatiana no esta
+#--- arrays
+#modificable
+lista = ["tatiana", "jonathan", "eduar", "lolo", 20]
+print(lista[3])
+
